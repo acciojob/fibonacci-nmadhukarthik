e1dbef238622ec7b1e43911eb1490ffcc41e99fa
Madhu <142790662+nmadhukarthik@users.noreply.github.com>
2024-03-11 11:16:10 +0800

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function fibonacci(num) {
 // your code here
+	if (num==1) 
+	{ return 0 }
+
+	if(num==2)
+	{ return 1 }
+		
+	let fib = fibonacci(num-1) + fibonacci(num-2)
+	return fib;
 }
 
 module.exports = fibonacci;
