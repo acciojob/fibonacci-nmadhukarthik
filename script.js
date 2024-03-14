99f5ed0fcada00a4bd2a32050d343c4c1ce71a94
Madhu <142790662+nmadhukarthik@users.noreply.github.com>
2024-03-15 00:08:01 +0800

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
 function fibonacci(num) {
 // your code here
-	if(num<=0)
-	{ return 0 }
-		
-	else if (num==1) 
+	if(num<=1)
 	{ return 0 }
 
 	else if(num==2)
 	{ return 1 }
 
 	else
-	{
-		let fib = fibonacci(num-1) + fibonacci(num-2)
-		return fib;
-	}
-	
+	{ return (fibonacci(num-1) + fibonacci(num-2)) }
 }
 
 module.exports = fibonacci;
