f70d79987ce8783c9996c436c05a92bfb3d4930e
Madhu <142790662+nmadhukarthik@users.noreply.github.com>
2024-03-17 11:47:00 +0800

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function fibonacci(num) {
 	for (let i = 2; i <= num; i++)
 	{ fibNum[i] =  fibNum[i-1] + fibNum[i-2] }
 
-	return fibNum[num]
+	return fibNum[num-1]
 }
 
 module.exports = fibonacci;
